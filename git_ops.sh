2b5ec73d0d83a5d99a0ff22caa7b2c74e162f106
Ken-XJ1 <kennethr1002@gmail.com>
2026-05-23 22:49:23 -0500

    fix: quitar preferencias del sistema, corregir asignacion tripulacion en embarcaciones y datos compras/tickets

--- git_ops.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-git add -A
-git commit -m "fix: quitar preferencias del sistema, corregir asignacion tripulacion en embarcaciones y datos compras/tickets"
-git push
-echo "GIT_DONE"
